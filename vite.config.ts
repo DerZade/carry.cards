542305e889dcc4f4d0cde2736ba7e86572e4b180
DerZade <DerZade@gmail.com>
2024-12-01 13:06:25 +0100

    chore: :technologist: add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,26 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+export default defineConfig(c => ({
+    plugins: [
+        vue(),
+        vueDevTools(),
+        c.mode === 'test'
+            ? false
+            : checker({
+                  enableBuild: false,
+                  eslint: {
+                      lintCommand: 'eslint .',
+                      useFlatConfig: true,
+                  },
+              }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
-});
+}));
