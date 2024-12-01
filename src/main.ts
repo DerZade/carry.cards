0827b536f63e9261ba4e60e6e1b12e1719d0ebcb
DerZade <DerZade@gmail.com>
2024-12-01 14:00:02 +0100

    chore: :building_construction: setup tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from './App.vue';
 import router from './plugins/router';
 import i18n from './plugins/i18n';
 
+import './assets/styles/index.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
