ddc76b362ccadf78cbd687e160fcba52be59d2d9
DerZade <derzade@gmail.com>
2026-01-30 13:33:08 +0100

    chore: :wrench: setup i18n

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    vueI18n({
+      include: fileURLToPath(new URL('./src/i18n/locales/**', import.meta.url)),
+      dropMessageCompiler: true,
+      strictMessage: false,
+      escapeHtml: false,
+      compositionOnly: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
