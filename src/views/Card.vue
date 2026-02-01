a52543c5840197b7aeeef17d50ee3fcd4431ba61
DerZade <derzade@gmail.com>
2026-02-01 01:10:21 +0100

    feat: :sparkles: add ability to show code in full screen

--- src/views/Card.vue
+++ src/views/Card.vue
@@ -1,9 +1,24 @@
 <template>
-  <div v-if="card" class="p-4">
+  <template v-if="!card">
+    <Spinner class="m-4" />
+  </template>
+  <div
+    v-else-if="fullscreen"
+    class="fixed inset-0 bg-red isolate z-100 overflow-hidden"
+    @click="fullscreen = false"
+  >
+    <RenderedCode
+      :format="card.format"
+      :rawValue="card.rawValue"
+      class="h-dvw w-dvh origin-top-left transform-[rotate(90deg)_translateY(-100%)]"
+    />
+  </div>
+  <div v-else class="p-4">
     <div
       class="aspect-(--card-aspect-ratio) card-bg rounded-3xl overflow-hidden relative card-transition"
     >
       <RenderedCode
+        @click="fullscreen = true"
         :format="card.format"
         :rawValue="card.rawValue"
         class="absolute rounded-xl inset-x-[10%] inset-y-[calc(10%*var(--card-aspect-ratio))]"
@@ -15,9 +30,10 @@
 <script setup lang="ts">
 import { useCardsStore } from '@/stores/cards'
 import { storeToRefs } from 'pinia'
-import { computed, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 import RenderedCode from '@/components/RenderedCode.vue'
+import Spinner from '@/components/Spinner.vue'
 
 const props = defineProps<{
   id: string
@@ -41,6 +57,8 @@ watch(
   },
   { immediate: true },
 )
+
+const fullscreen = ref(false)
 </script>
 
 <style scoped>
